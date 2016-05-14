4f2e6bf995728e000ec985c8caa8e8efbfef936e
User <User@HayleyCom>
2016-05-14 18:44:40 +0900

    update portfolio

---
+++ gittest_jung/WebContent/common/script/resizewindow.js
@@ -0,0 +1,12 @@
+﻿$(window).ready(function () {
+
+    //var iwinsize = $(window).height();
+    var iwinsize = window.innerHeight + 80;
+    var idocsize = $(document).height();
+
+    if (iwinsize > idocsize) {
+        $(".resize_box").css("height", iwinsize - idocsize);
+        //$(".resize_inbox").css("background-color", "red");
+    }
+
+});
